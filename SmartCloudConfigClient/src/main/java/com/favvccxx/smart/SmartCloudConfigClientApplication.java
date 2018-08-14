b5cfea69d46685b2b9e94b1eb23e02bd8840d004
chenxu <xu.chen.cx@siemens.com>
2018-08-14 16:34:19 +0800

    test refresh

--- SmartCloudConfigClient/src/main/java/com/favvccxx/smart/SmartCloudConfigClientApplication.java
+++ SmartCloudConfigClient/src/main/java/com/favvccxx/smart/SmartCloudConfigClientApplication.java
@@ -1,11 +1,19 @@
 package com.favvccxx.smart;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
-//@EnableConfigServer
+
 @SpringBootApplication
 public class SmartCloudConfigClientApplication {
+	
+	@Autowired
+	void setEnviroment(Environment env) {
+		System.out.println("my-config.appName from env: " 
+			+ env.getProperty("smart.from"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartCloudConfigClientApplication.class, args);
